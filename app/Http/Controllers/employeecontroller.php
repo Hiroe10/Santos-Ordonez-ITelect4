257e6d9b972b249a31ea3ad7ecc3dd7eb2510da5
Kristan Paul Ordonez <kristan2903@gmail.com>
2025-05-30 09:09:59 +0800

    edit,delete

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -12,7 +12,7 @@ class employeecontroller extends Controller
     {   
         $employees = employee::get();
 
-        return view ('employee.index',compact('employees'));
+        return view ('employee.index');
     }
 
     public function store(Request $request){
@@ -30,10 +30,38 @@ class employeecontroller extends Controller
 
     }
 
-    public function create(){
-        return view('employee.create');
+    public function edit(int $id)
+    {
+        $employees = employee::findOrFail($id);
+        return view('employee.edit',compact('employees'));
     }
 
+    public function update(Request $request,int $id)
+    {
+        $request->validate([
+             'fname' => "required|max:255|string",
+            'lname' => "required|max:255|string",
+            'midname' => "required|max:255|string",
+            'age' => "required|integer",
+            'address' => "required|max:255|string",
+            'zip' => "required|integer"
+
+        ]);
+
+        employee::findOrFail($id)->update($request->all());
+        return redirect()->back()->with('status','employee updated succesfully');
+
+
+    }
+
+    public function destroy(int $id)
+    {
+        $employees=employee::findOrFail($id);
+        $employees->delete();
+        return redirect()->back()->with('status','employee deleted');
+
+
+    }
         
             // employee::findOrFail($id)->update($request->all());
             // return redirect ()->back()->with('status','Employee Updated Successfully!');
